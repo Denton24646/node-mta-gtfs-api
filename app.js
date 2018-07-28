efd636e6b94a86f2ec6d0defc01615e791df9321
Denton24646 <Denton24646@gmail.com>
2018-07-28 17:39:11 -0400

    #1 move to alert
    move to alert

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ request(requestSettings, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
     feed.entity.forEach(function(entity) {
-      if (entity.trip_update) {
-        console.log(entity.trip_update);
-      }
+        console.log('alert: '+ entity.alert);
     });
   }
 });
